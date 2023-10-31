ca9ef0702413c08eeb1274e2f2dd3da8e84dead2
A-nil23 <146312515+A-nil23@users.noreply.github.com>
2023-10-30 22:17:02 -0700

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -13,3 +13,23 @@ update-alternatives --config java
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
 systemctl start jenkins.service
 systemctl status jenkins.service
+
+
+
+sudo yum update
+    2  mkdir paytm
+    3  cd paytm
+    4  yum install git
+    5  ll
+    6  ls
+    7  ll -a
+    8  git init
+    9  touch jenkins.sh
+   10  vim jenkins.sh
+   11  git add jenkins.sh
+   12  git status
+   13  git commit -m "v-1" jenkins.sh
+   14  git remote add origin https://github.com/A-nil23/A-nil23.git
+   15  git checkout -b jenkins.sh
+   16  git branch
+   17  git push origin jenkins.sh
